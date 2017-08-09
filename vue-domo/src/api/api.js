a4dab247486e67eab6fcb5ac36cf4c238653cfe2
USER-20161202QK\Administrator <805981308@qq.com>
2017-08-09 15:51:51 +0800

    修改apijs

--- vue-domo/src/api/api.js
+++ vue-domo/src/api/api.js
@@ -3,4 +3,6 @@ import axios from 'axios';
 
 export const getUser = (col,params) => { return axios.post(`http://localhost:3000${col}`, params)}
 
+export const Post = (col,params) => { return axios.post(`http://localhost:3000${col}`, params)}
+
 
